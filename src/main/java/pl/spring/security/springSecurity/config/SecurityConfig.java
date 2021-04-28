8559e469cf43e40de374f6268448138e66bd439a
Pawel Janusz <marek80k@wp.pl>
2021-04-28 19:04:59 +0200

    security configuration

---
+++ src/main/java/pl/spring/security/springSecurity/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package pl.spring.security.springSecurity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/status")
+                    .hasAnyAuthority("ROLE_ADMIN")
+                .anyRequest()
+                    .permitAll()
+                .and()
+                .csrf().disable()
+                .formLogin()
+                .defaultSuccessUrl("/status");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user@test.pl")
+                .password(passwordEncoder.encode("user"))
+                .roles("USER")
+            .and()
+                .withUser("admin@test.pl")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN");
+    }
+}
