499c7cf6afa00eca3b446abc34272ad1ba25e4ab
Pawel Janusz <marek80k@wp.pl>
2021-05-02 15:17:17 +0200

    model

---
+++ src/main/java/pl/spring/security/springSecurity/model/Email.java
@@ -0,0 +1,10 @@
+package pl.spring.security.springSecurity.model;
+
+public class Email {
+
+    private String email;
+
+    public Email(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/pl/spring/security/springSecurity/model/User.java
@@ -0,0 +1,11 @@
+package pl.spring.security.springSecurity.model;
+
+public class User {
+
+    private String name;
+    private String surname;
+    private String email;
+
+    int age;
+
+}
